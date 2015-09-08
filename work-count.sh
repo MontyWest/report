a885b3c7adf17411a25ec7c6da769c3a234f1f82
MontyWest <monty.p.west@gmail.com>
2015-09-08 18:55:00 +0100

    split into several file, appendix added, intro done

---
+++ work-count.sh
@@ -0,0 +1,2 @@
+cd ~/development/Project/Report
+texcount ReportV2.tex Chap1Intro.tex Chap2ExistingWork.tex Chap3ProjSpec.tex Chap4Agent.tex Chap5LevelPlay.tex Chap6Learning.tex Chap7Results.tex Chap8Eval.tex
